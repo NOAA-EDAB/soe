c1f2b3853a2270a410369aa9accd55252eb966c3
Sarah Gaichas <sarah.gaichas@noaa.gov>
2024-01-17 19:57:33 -0500

    attempt Mark's forage quality/quantity WIP

---
+++ R/synthplottest.R
@@ -0,0 +1,31 @@
+#' plot forage_qual_quant
+#'
+#'
+
+load("/Users/sarahgaichas/Documents/Work/forageindex/survdat/Survdat.Rdata")
+
+pulldate <- survey$pullDate
+functioncall <- survey$functionCall
+survdat <- survey$survdat
+
+foragespp <- ecodata::energy_density |>
+  tidyr::separate(Var, into = c("Species", "Season", "Var"), sep = "/") |>
+  dplyr::select(Species) |>
+  dplyr::distinct()
+
+foragespp <- unname(unlist(foragespp))
+foragespp <- stringr::str_replace(foragespp, "Atl.", "Atlantic")
+
+foragespp <- stringr::str_replace(foragespp, "Sand lance", "Sandlance")
+foragespp <- stringr::str_remove(foragespp, " squid")
+
+foragespp <- stringr::str_to_upper(foragespp)
+
+foragelook <- ecodata::species_groupings |>
+  dplyr::filter(COMNAME %in% foragespp)
+
+
+foragequant <- survdat |>
+  dplyr::filter()
+
+
