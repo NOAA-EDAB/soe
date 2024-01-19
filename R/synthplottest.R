f26c4cc163e8ebb7cd1bd18e1429925f0008c647
Sarah Gaichas <sarah.gaichas@noaa.gov>
2024-01-19 11:32:57 -0500

    make it a function

--- R/synthplottest.R
+++ R/synthplottest.R
@@ -1,31 +1,43 @@
 #' plot forage_qual_quant
 #'
 #'
+#'
+#' @return ggplot object
+#'
+#'
+#' @export
 
-load("/Users/sarah.gaichas/Documents/0_Data/forageindex/survdat/Survdat.Rdata")
+plot_forage_qual_quant <- function() {
 
-pulldate <- survey$pullDate
-functioncall <- survey$functionCall
-survdat <- survey$survdat
+  survey <- readRDS(here::here("data-raw/survdat.rds"))
 
-foragespp <- ecodata::energy_density |>
-  tidyr::separate(Var, into = c("Species", "Season", "Var"), sep = "/") |>
-  dplyr::select(Species) |>
-  dplyr::distinct()
+  pulldate <- survey$pullDate
+  functioncall <- survey$functionCall
+  survdat <- survey$survdat
 
-foragespp <- unname(unlist(foragespp))
-foragespp <- stringr::str_replace(foragespp, "Atl.", "Atlantic")
+  foragespp <- ecodata::energy_density |>
+    tidyr::separate(Var, into = c("Species", "Season", "Var"), sep = "/") |>
+    dplyr::select(Species) |>
+    dplyr::distinct()
 
-foragespp <- stringr::str_replace(foragespp, "Sand lance", "Sandlance")
-foragespp <- stringr::str_remove(foragespp, " squid")
+  foragespp <- unname(unlist(foragespp))
+  foragespp <- stringr::str_replace(foragespp, "Atl.", "Atlantic")
 
-foragespp <- stringr::str_to_upper(foragespp)
+  foragespp <- stringr::str_replace(foragespp, "Sand lance", "Sandlance")
+  foragespp <- stringr::str_replace(foragespp, "Illex squid", "NORTHERN SHORTFIN SQUID")
+  foragespp <- stringr::str_replace(foragespp, "Loligo squid", "LONGFIN SQUID")
 
-foragelook <- ecodata::species_groupings |>
-  dplyr::filter(COMNAME %in% foragespp)
+  foragespp <- stringr::str_to_upper(foragespp)
+
+  foragelook <- ecodata::species_groupings |>
+    dplyr::filter(COMNAME %in% foragespp)
 
 
-foragequant <- survdat |>
-  dplyr::filter()
+  foragequant <- survdat |>
+    dplyr::filter()
 
+  p <- ggplot2::ggplot()
 
+  return(p)
+
+}
